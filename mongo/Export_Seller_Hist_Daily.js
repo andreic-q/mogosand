d0341a993f04d5845911122abc52fd4711b3e22e
Andrei Canache <andreicanache@qogita.com>
2022-06-28 12:50:17 +0100

    enhance parquet archiving and trigger

--- mongo/Export_Seller_Hist_Daily.js
+++ mongo/Export_Seller_Hist_Daily.js
@@ -1,60 +1,67 @@
 exports = async function () {
 
-  const datalake = context.services.get("FedDB1");
-  const coll = "sellerHistArchive";
-  const jobLogs = datalake.db("test").collection("logs");
-  const sellerHist = datalake.db("biquit").collection(coll);
+  try {
+    const datalake = context.services.get("FedDB1");
+    const coll = "sellerHistArchive";
+    const jobLogs = datalake.db("test").collection("logs");
+    const sellerHist = datalake.db("biquit").collection(coll);
+    
+    const result =await jobLogs.find({"status" : {"$exists":0}},{"_id": 1,  "start_date": 1 },{"$orderby": { "started_at" : -1 }})
+          .toArray();
+    result.forEach(doc =>  start_dt_string = doc.start_date);
+    
+    // console.log(start_dt_string);
+    // console.log(new Date(start_dt_string));
+    // const result =await logsColl.findOne({ "start_date": "2021-07-20"});
+    // console.log(result._id);
+    
+    // nextRunDoc.forEach( function(docs) { print( "Next Run: " + docs.start_date ) } ); // pass in a JS function
+    //new Date(Date.now()); 
+    
+    const end_date = getNextDay(new Date(start_dt_string))
+    var recon_date = splitDate(start_dt_string);
+    const out_path=recon_date.join('/');
+    const outFileName = recon_date[0]+recon_date[1]+recon_date[2];
+    console.log(out_path + "/" + outFileName);
   
-  const result =await jobLogs.find({"status" : {"$exists":0}},{"_id": 1,  "start_date": 1 },{"$orderby": { "started_at" : -1 }})
-        .toArray();
-  result.forEach(doc =>  start_dt_string = doc.start_date);
+    const pipeline = [
+        {
+          '$match': 
+              {datetime: 
+                      {
+                      '$gte': new Date(start_dt_string),
+                      '$lt':  end_date
+                      }
+              }
+        }
+        ,{'$limit': 50}
+        , {
+          '$out': {
+              's3': {
+                  'bucket': 'mongo-atlas-export-test', 
+                  'region': 'eu-west-2', 
+                  'filename': {'$concat':[ coll, "/"
+                                            ,out_path
+                                            ,"/"
+                                            ,outFileName]
+                              }, 
+                  'format': {
+                      'name': 'parquet', 
+                      'maxFileSize': '2GB',
+                      'columnCompression': 'gzip'
+                  }
+              }
+          }
+      }
+      ];
   
-  // console.log(start_dt_string);
-  // console.log(new Date(start_dt_string));
-  // const result =await logsColl.findOne({ "start_date": "2021-07-20"});
-  // console.log(result._id);
-   
-  // nextRunDoc.forEach( function(docs) { print( "Next Run: " + docs.start_date ) } ); // pass in a JS function
-  //new Date(Date.now()); 
-  
-  const end_date = getNextDay(new Date(start_dt_string))
-  var recon_date = splitDate(start_dt_string);
-  const out_path=recon_date.join('/');
-  const outFileName = recon_date[0]+recon_date[1]+recon_date[2];
-  console.log(out_path + "/" + outFileName);
- 
-  const pipeline = [
-       {
-         '$match': 
-             {datetime: 
-                     {
-                     '$gte': new Date(start_dt_string),
-                     '$lt':  end_date
-                     }
-             }
-       }
-       ,{'$limit': 50}
-       , {
-         '$out': {
-             's3': {
-                 'bucket': 'mongo-atlas-export-test', 
-                 'region': 'eu-west-2', 
-                 'filename': {'$concat':[ coll, "/"
-                                          ,out_path
-                                          ,"/"
-                                          ,outFileName]
-                             }, 
-                 'format': {
-                     'name': 'parquet', 
-                     'maxFileSize': '1GB',
-                     'columnCompression': 'gzip'
-                 }
-             }
-         }
-     }
-    ];
- 
-return sellerHist.aggregate(pipeline,{ allowDiskUse: true });
+    return sellerHist.aggregate(pipeline,{ allowDiskUse: true });
+  } catch (error) {
+      if (error instanceof MongoServerError) {
+        console.log(`Error worth logging: ${error}`); // special case for some reason
+      }
+    throw error; // still want to crash
+  }
 }
  
 
